e6bb53f0b2cc5005b2489e37fbbcba025d6969cb
unknown <mcalumgannon@gmail.com>
2023-10-12 19:36:13 +0100

    created utils folder

---
+++ utils/int_to_list.py
@@ -0,0 +1,12 @@
+
+
+
+def intToList(int_value):
+    n = 0
+    list = []
+    while n < int_value:
+        n += 1
+        list.append(n)
+    return list
+
+
---
+++ utils/square_number.py
@@ -0,0 +1,4 @@
+def squareNumber(int_value):
+    int_value = int_value * int_value
+
+    return int_value
